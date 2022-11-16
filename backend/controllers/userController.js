46f8e94d9ac3898f2b3c75066672ae2580be149a
prasannakarki77 <prasanna.karki77@gmail.com>
2022-11-16 20:00:39 +0545

    folder structure and db connection setup

---
+++ backend/controllers/userController.js
@@ -0,0 +1,25 @@
+const asyncHandler = require("express-async-handler");
+
+// @desc Register a new user
+// @route /api/users
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, passowrd } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please include all fields");
+  }
+});
+
+// @desc Login a user
+// @route /api/users/login
+// @access Public
+const loginUser = asyncHandler(async (req, res) => {
+  res.send("Login Route");
+});
+
+module.exports = {
+  registerUser,
+  loginUser,
+};
