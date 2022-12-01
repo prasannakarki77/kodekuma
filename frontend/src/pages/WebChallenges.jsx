3188d0142b19fa68ff2309c6f5201de555e699ea
prasannakarki77 <prasanna.karki77@gmail.com>
2022-12-01 15:22:22 +0545

    added tabs in challneges page

--- frontend/src/pages/WebChallenges.jsx
+++ frontend/src/pages/WebChallenges.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { CgWebsite } from "react-icons/cg";
 import { Link, Outlet } from "react-router-dom";
 const WebChallenges = () => {
+  const [currentTab, setCurrentTab] = useState("challenges");
+  const tabHandler = (tab) => setCurrentTab(tab);
   return (
     <div className=" md:px-10 lg:px-20 px-3 ">
       <h1 className="font-cubano mb-10 text-2xl sm:text-3xl text-primary-content flex items-center">
@@ -14,11 +16,22 @@ const WebChallenges = () => {
       <div className="tabs font-cubano mb-9">
         <Link
           to="/web_challenges/"
-          className="tab tab-bordered tab-active  tab-lg "
+          className={`tab tab-bordered   tab-lg ${
+            currentTab === "challenges" && "tab-active"
+          }  `}
+          onClick={() => tabHandler("challenges")}
         >
           Challenges
         </Link>
-        <Link className="tab tab-bordered  tab-lg">Solutions</Link>
+        <Link
+          to="/web_challenges/solutions"
+          className={`tab tab-bordered   tab-lg ${
+            currentTab === "solutions" && "tab-active"
+          }  `}
+          onClick={() => tabHandler("solutions")}
+        >
+          Solutions
+        </Link>
       </div>
       <div>
         <Outlet />
