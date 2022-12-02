bb745d6c214e161869364c43a88140a32edffa72
prasannakarki77 <prasanna.karki77@gmail.com>
2022-12-02 12:14:59 +0545

    added styled challenge level component

--- frontend/src/components/ChallengeCard.jsx
+++ frontend/src/components/ChallengeCard.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import challenge_img from "../assets/challenge_img.png";
+import styled from "styled-components";
+import { css } from "styled-components";
 const ChallengeCard = ({ challengeItem, number }) => {
   const {
     _id,
@@ -23,13 +25,13 @@ const ChallengeCard = ({ challengeItem, number }) => {
         />
       </figure>
       <div className="card-body">
-        <div className="badge badge-success ml-auto capitalize">
-          {difficulty}
+        <div className=" ml-auto capitalize font-poppins">
+          <ChallengeLevel level={difficulty}> {difficulty}</ChallengeLevel>
         </div>
         <h2 className="card-title font-cubano text-primary-content tracking-wider">
           #{number} {challenge}
         </h2>
-        <p className=" mb-5">{desc}</p>
+        <p className=" mb-5 font-semibold ">{desc}</p>
         <Link>
           {" "}
           <button className="font-cubano text-base w-full btn hover:bg-green-600  tracking-widest leading-5 bg-challenge  text-white sm:mb-0 ">
@@ -40,5 +42,20 @@ const ChallengeCard = ({ challengeItem, number }) => {
     </div>
   );
 };
+
+const ChallengeLevel = styled.div`
+  display: inline-block;
+  text-transform: capitalize;
+  border-radius: 29px;
+  padding: 7px 13px;
+  background: ${({ level }) =>
+    (level === "beginner" && "#5DB355") ||
+    (level === "intermediate" && "#721792") ||
+    (level === "advanced" && "#3C7154") ||
+    (level === "expert" && "#C51717")};
+  color: #fff;
+  font-weight: 500;
+  font-size: 14px;
+`;
 
 export default ChallengeCard;
